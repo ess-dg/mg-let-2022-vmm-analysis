5288a766d0b0e16235ffb9cd09d798a00b5668cf
Alexander Backis <alexander.backis@gmail.com>
2022-03-25 16:53:06 +0100

    Added debugging tools

--- plotting/mg_vmm_debug.py
+++ plotting/mg_vmm_debug.py
@@ -27,16 +27,65 @@ def plot_channel_histograms(df, file_name):
         for vmm_id in range(number_vmms_per_fen):
             ax = axes[fen_id][vmm_id // 2]
             df_vmm = df[(df.fen == fen_id) & (df.vmm == vmm_id)]
-            ax.hist(df_vmm.channel, color=colors[vmm_id], histtype='step',
-                    range=[-0.5, 63.5], bins=64, label='VMM %d' % vmm_id,
-                    linestyle=linestyles[vmm_id])
-            ax.legend(title='Hybrid %d' % (vmm_id // 2), ncol=2)
+            #hist, bins, __ = ax.hist(df_vmm.channel, color=colors[vmm_id], histtype='step',
+            #                         range=[-0.5, 63.5], bins=64, label='VMM %d' % (vmm_id%2),
+            #                         linestyle=linestyles[vmm_id])
+            hist, bins = np.histogram(df_vmm.channel, range=[-0.5, 63.5], bins=64)
+            bin_centers = (bins[:-1] + bins[1:]) / 2
+            ax.bar(bin_centers, hist, align='center', alpha=0.5, label='VMM %d' % (vmm_id%2))
+            ax.legend(title='Hybrid %d' % (vmm_id // 2), ncol=2, loc=4)
     ax3.set_xlabel('Channel')
     ax4.set_xlabel('Channel')
     ax1.set_ylabel('Counts')
     ax3.set_ylabel('Counts')
     ax1.set_title('FEN 0')
     ax2.set_title('FEN 1')
+    plt.tight_layout()
+    
+def plot_channel_histograms_large(df, file_name, lower_threshold=-np.inf, higher_threshold=np.inf):
+    """
+    
+    """
+    # Declare parameters
+    number_rings = 2
+    number_fens = 1
+    number_vmms_per_fen = 4
+    # Plot data 
+    fig, ((ax1, ax2, ax3, ax4), (ax5, ax6, ax7, ax8)) = plt.subplots(nrows=2, ncols=4,
+                                                                     sharey=True, sharex=True)
+    plt.suptitle(file_name)
+    axes = [[ax1, ax5], [ax2, ax6], [ax3, ax7], [ax4, ax8]]
+    colors = ['red', 'blue']
+    linestyles = ['solid', 'dotted', 'dashed', 'dashdot']
+    for ring_id in range(number_rings):
+        for vmm_id in range(number_vmms_per_fen):
+            ax = axes[2*ring_id + vmm_id // 2][vmm_id % 2]
+            ax.set_title('Ring ID: %d\nHybrid: %d, VMM: %d' % (ring_id, (vmm_id // 2), (vmm_id % 2)))
+            df_vmm = df[(df.ring // 2 == ring_id) & (df.vmm == vmm_id)]
+            hist, bins = np.histogram(df_vmm.channel, range=[-0.5, 63.5], bins=64)
+            lower_idxs = np.where(hist < lower_threshold)
+            higher_idxs = np.where(hist > higher_threshold)
+            if (len(lower_idxs[0]) != 64) or (len(higher_idxs[0]) != 64):
+                print('-------------------------------------------------------------------')
+                print('Ring ID: %d, Hybrid: %d, VMM: %d' % (ring_id, (vmm_id // 2), (vmm_id % 2)))
+                print('-------------------------------------------------------------------')
+                print('Number silent channels: %d' % len(lower_idxs[0]))
+                print('Number noisy channels: %d' % len(higher_idxs[0]))
+                if(len(lower_idxs[0]) != 64):
+                    print('Quiet channels', lower_idxs[0])
+                if(len(higher_idxs[0]) != 64):
+                    print('Noisy channels', higher_idxs[0])
+                print('###################################################################')
+            bin_centers = (bins[:-1] + bins[1:]) / 2
+            ax.bar(bin_centers, hist, align='center', alpha=0.5, label='VMM %d' % (vmm_id%2), color=colors[vmm_id % 2])
+            #ax.legend(title='Hybrid %d' % (vmm_id // 2), ncol=2, loc=4)
+    ax5.set_xlabel('Channel')
+    ax6.set_xlabel('Channel')
+    ax7.set_xlabel('Channel')
+    ax8.set_xlabel('Channel')
+    ax1.set_ylabel('Counts')
+    ax5.set_ylabel('Counts')
+    fig.set_size_inches(13, 7)
     plt.tight_layout()
     
 
